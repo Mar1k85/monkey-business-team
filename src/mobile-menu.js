504100bb1f6a2b22d59e2e557c783d9619103828
Kateryna Kovalchuk <kg0025789@gmail.com>
2022-11-01 23:16:38 +0200

    mob-js

--- src/mobile-menu.js
+++ src/mobile-menu.js
@@ -3,12 +3,14 @@
   const openMenuBtn = document.querySelector('.js-open-menu');
   const closeMenuBtn = document.querySelector('.js-close-menu');
   const bodyScrollLock = document.querySelector("body");
+  
   function toggleMenu() {
       const isMenuOpen = openMenuBtn.getAttribute('aria-expanded') === 'true' || false;
       openMenuBtn.setAttribute('aria-expanded', !isMenuOpen);
       mobileMenu.classList.toggle('is-open');
       bodyScrollLock.classList.toggle('no-scroll');
   }
+  
   openMenuBtn.addEventListener('click', toggleMenu);
   closeMenuBtn.addEventListener('click', toggleMenu);
   // Close the mobile menu on wider screens if the device orientation changes
